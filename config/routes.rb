36e1296373dd16e8701be47e68407622ed800d7f
Harold A. Speights <harold.a.speights@gmail.com>
2015-09-08 21:22:13 -0400

    Assignment 35 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :question
+
+  resources :questions
+
   resources :posts
 
   get "welcome/index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823202152) do
+ActiveRecord::Schema.define(version: 20150906175026) do
+
+  create_table "advertisements", force: true do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -25,6 +33,14 @@ ActiveRecord::Schema.define(version: 20150823202152) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "questions", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
