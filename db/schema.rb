ee9626d9e8775ada11ef05200e27b666289af5d0
Harold A. Speights <harold.a.speights@gmail.com>
2015-09-20 21:33:12 -0400

    Checkpoint 38 completed, associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916034004) do
+ActiveRecord::Schema.define(version: 20150921010105) do
 
   create_table "advertisements", force: true do |t|
     t.string   "title"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20150916034004) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: true do |t|
     t.string   "title"
