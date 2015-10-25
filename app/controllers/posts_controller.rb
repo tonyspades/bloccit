86022532941fb5a2f19bf7642f7e9443a437e400
Harold A. Speights <harold.a.speights@gmail.com>
2015-10-25 17:10:59 -0400

    Checkpoint 47 completed, Destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,8 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @comments = @post.comments.to_a
     @comment = @post.comments.build
-    authorize @comment
-    authorize @comments
+    #authorize @comment
+    #authorize @comments
   end
 
   def new
@@ -46,6 +46,20 @@ class PostsController < ApplicationController
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
+     end
+   end
+
+   def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     authorize @post
+ 
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to @topic
+     else
+       flash[:error] = "There was an error deleting the post."
+       render :show
      end
    end
 
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -40,4 +40,17 @@ class TopicsController < ApplicationController
        render :edit
      end
    end
+
+   def destroy
+     @topic = Topic.find(params[:id])
+ 
+     authorize @topic
+     if @topic.destroy
+       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+       redirect_to topics_path
+     else
+       flash[:error] = "There was an error deleting the topic."
+       render :show
+     end
+   end
 end
