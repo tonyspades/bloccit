60dc706a4023de8adcf81cc3a324e4e0fc99846c
Harold A. Speights <harold.a.speights@gmail.com>
2015-10-25 17:44:27 -0400

    Completed assignment 47, Destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
   resources :topics do
-     resources :posts, except: [:index] do
-       resources :comments, only: [:create, :destroy]
-     end
-   end
+    resources :posts, except: [:index]
+  end
+ 
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
 
   get "welcome/index"
 
